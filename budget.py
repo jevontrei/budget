8402f88620bf2a1ab2a15d4d14afe3e95c1e755b
jevontrei <160815182+jevontrei@users.noreply.github.com>
2025-04-01 09:45:02 +1000

    add MU's instructions

--- budget.py
+++ budget.py
@@ -21,10 +21,17 @@ headers = {"Authorization": f"Bearer {api_key}"}
 # r = requests.get("https://api.up.com.au/api/v1/transactions/4ced5c33-1d57-4747-9038-de8fe0367539", headers=headers)
 # print(f"\n{r.text}\n")
 
+# get a transaction's category
+# r = requests.get("https://api.up.com.au/api/v1/transactions/?filter[category]=health-and-medical", headers=headers)
+# print(f"\n{r.text}\n")
+
 # # get a transaction's category
 r = requests.get("https://api.up.com.au/api/v1/transactions/4ced5c33-1d57-4747-9038-de8fe0367539", headers=headers)
 print(f"\n{r.text}\n")
 
-# get a transaction's category
-# r = requests.get("https://api.up.com.au/api/v1/transactions/?filter[category]=health-and-medical", headers=headers)
-# print(f"\n{r.text}\n")
+# 1. find out what pydantic is
+# 2. why might you want to use it
+# 3. create types for the up apis
+# 4. make fn that takes an enum and returns the appropirate data type
+
+def get_data(data:str)->dict:
---
+++ get_data.ts
@@ -0,0 +1,11 @@
+enum APITYPE {
+    ACCOUNTS
+    TRANSACTIONS
+    PING 
+}
+
+async function get_data(data: UpApiType): UpReturnType {
+    switch data.ACCOUNTS { 
+        return fetch("...from accounts")
+    }
+}
