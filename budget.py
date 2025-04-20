fda7aa8a935dc119bb0235ea07b787d3a50fad62
jevontrei <160815182+jevontrei@users.noreply.github.com>
2025-04-20 13:02:32 +1000

    write get_accounts()

--- budget.py
+++ budget.py
@@ -15,7 +15,7 @@ from typing import Any
 from pydantic import BaseModel
 from enum import Enum
 
-from utils import add_tag, get_a_transaction  # , get_accounts, get_transactions
+from utils import add_tag, get_a_transaction, get_transactions, get_accounts, get_an_account
 # from get_data import get_data
 
 os.system("clear")
@@ -26,17 +26,17 @@ def main():
     while True:
         print()
         print("-" * 50)
-        print("Hi! What do you wanna do?")
-        print("0. Check balance")
+        print("Hi! What do you wanna do?\n")
+        print("0. Check spending balance")
         print("1. Get accounts")
+        print("10. Get an account")
         print("2. Get a transaction")
         print("20. Get transactions")
         print("3. Add a tag to a transaction")
         print("4. Clear terminal")
         print("5. What's the time?")
         print("6. Read me a poem")
-        print("7. Exit")
-        print()
+        print("7. Exit\n")
 
         selection = input("Select a number: ")
         selection = int(selection.strip())
@@ -44,22 +44,69 @@ def main():
 
         match selection:
             case 0:
-                ...
-                # get_balance()
+                # Firstly, present all accounts and let user choose one
+                accounts = get_accounts()
+                if not accounts:
+                    print("Oops")
+                else:
+                    count = 1
+                    print("Available accounts:")
+                    for account in accounts["data"]:
+                        print(f"{count}. {account['attributes']['displayName']}")
+                        count += 1
+                    print()
+
+                selected_account = input("Select an account (1, 2, 3 etc): ")
+                selected_account = int(selected_account.strip())
+                print()
+
+                if selected_account in range(1, len(accounts) + 1):
+                    # response = get_balance(selected_account)
+                    account = accounts["data"][selected_account - 1]
+                    print(f"Balance: {account['attributes']['balance']['value']} {account['attributes']['balance']['currencyCode']}")
+                else:
+                    print("Sorry, that number doesn't make sense to me.")
+                    continue
+
+                # if not response:
+                #     print("No response.")
+                # else:
+                #     print(f"{response}")
 
             case 1:
-                ...
-                # get_accounts()
+                response = get_accounts()
+                if not response:
+                    print("No response.")
+                else:
+                    for account in response["data"]:
+                        print(f"\n{account}")
+
+            case 10:
+                response = get_an_account()
+                if not response:
+                    print("No response.")
+                else:
+                    print(f"{response}")
 
             case 2:
                 transactionId = input("Enter a transactionId, or press Enter to use the default: ")
                 transactionId = transactionId or "207f6db9-888d-4bd9-afd9-73ec3e02c382"
 
-                get_a_transaction(transactionId)
+                response = get_a_transaction(transactionId)
+
+                if not response:
+                    print("No response.")
+                else:
+                    print(f"{response}")
 
             case 20:
-                ...
-                # get_transactions()
+                response = get_transactions()
+
+                if not response:
+                    print("No response.")
+                else:
+                    for transaction in response["data"]:
+                        print(f"\n{transaction}")
 
             case 3:
                 transactionId = input("Enter a transactionId, or press Enter to use a default: ")
@@ -69,14 +116,17 @@ def main():
                 tagId = str(tagId) or f"test{int(time.time())}"
                 tag = {"data": [{"type": "tags", "id": tagId}]}
 
-                response = add_tag(transactionId=transactionId, tag=tag)
+                add_tag(transactionId=transactionId, tag=tag)
+                # response = add_tag(transactionId=transactionId, tag=tag)
                 print()
 
-                print("Now let's see our hard work...")
-                print()
+                print("Now let's see our hard work...\n")
                 tags = get_a_transaction(transactionId)["data"]["relationships"]["tags"]["data"]
-                for tag in tags:
-                    print(tag)
+                if not tags:
+                    print("Oops")
+                else:
+                    for tag in tags:
+                        print(tag)
 
             case 4:
                 os.system("clear")
